3bff9b09000ee33e206f6781ec14aece31199f17
alwinshabu2-web <alwinshabu2@gmail.com>
2026-06-17 12:06:27 +0530

    Add files via upload

---
+++ basics/conditionalexpression.py
@@ -0,0 +1,12 @@
+age=int(input("enetr your age:"))
+
+if(age<18):
+    print("you are not elegible")
+else:
+    print("you are eligible")
+    
+#in statment
+
+a="alwin is a good boy"
+print("alwin" in a)
+print("aaron" in a)
